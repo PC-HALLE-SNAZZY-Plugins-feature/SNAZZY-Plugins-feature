f4691427ec2431ca45335f2df0a97d37bab88a15
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-05-19 03:38:20 +0100

    added the infinty scroll for swiper

--- slides.js
+++ slides.js
@@ -13,30 +13,41 @@ search_plugin.addEventListener("input", function(event) {
 function startCarousel() {
 
     let carousel = document.querySelector(".carousel");
-    let carouselFirstChild = carousel.firstElementChild;
+    let carouselFirstChild = carousel.querySelector(".slider");
     let slides = carousel.querySelectorAll(".slide");
-  
+    let A_slides =  Array.from(slides);
     let currentIndex = 0; 
+
+    const lastSlide = A_slides[A_slides.length-1].cloneNode(true);
+    // A_slides.unshift(lastSlide);
+    carouselFirstChild.insertBefore(lastSlide, carouselFirstChild.firstChild);
     
     function moveNext() {
-        // let width = slides[currentIndex].offsetWidth;
         
-        let width = slides[currentIndex].offsetWidth;
-        carouselFirstChild.scrollLeft += width;
-    slides[currentIndex].classList.remove("active");
-    ArrayFromSlides = Array.from(slides); 
-    let activeSlide = slides[currentIndex];
-    let indexToRemove = ArrayFromSlides.indexOf(activeSlide);
-    ArrayFromSlides.splice(indexToRemove, 1);
-    ArrayFromSlides.push(slides[currentIndex]);
-    carouselFirstChild.appendChild(slides[currentIndex]);
+        let width = currentIndex === 0 ? 170 : 180;
 
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add("active");
+        A_slides[currentIndex].classList.remove("active");
     
-//  if (currentIndex === 0) {
-//         carousel.scrollLeft = 0;
-//     } 
+        carouselFirstChild.scrollLeft += width;
+    
+        
+        const clonedSlide = A_slides[currentIndex].cloneNode(true);
+        A_slides.push(clonedSlide);
+        carouselFirstChild.appendChild(clonedSlide);
+        if(A_slides.length > slides.length + 2){
+            
+            carouselFirstChild.removeChild(A_slides[0]);
+            carouselFirstChild.scrollLeft += -width;
+            A_slides.shift();
+            currentIndex--;
+            console.log('A_slides',A_slides);
+            
+        }
+
+        currentIndex = (currentIndex + 1)  ;
+
+        A_slides[currentIndex].classList.add("active");
+    
     }
     
     let intervalId = setInterval(moveNext, 3000); 
@@ -46,7 +57,8 @@ function startCarousel() {
     });
     
     carousel.addEventListener("mouseleave", function() {
-        intervalId = setInterval(moveNext, 25000);
+        intervalId = setInterval(moveNext, 3000); 
+
     });
 }
 
