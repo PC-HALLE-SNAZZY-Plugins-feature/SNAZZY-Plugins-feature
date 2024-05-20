026cda20e64d5ccda8bb3fef76d93c538d9624d0
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-05-20 14:49:20 +0100

    added  the event click  for each items in the slides

--- slides.js
+++ slides.js
@@ -1,12 +1,12 @@
 
 // handle the visibility of the search container
 const search_plugin = document.getElementById("search-plugins");
-search_plugin.addEventListener("input", function(event) {
-    if(search_plugin.value.length > 0)
-    document.getElementById("search-container").style.display = 'block ';
+search_plugin.addEventListener("input", function (event) {
+    if (search_plugin.value.length > 0)
+        document.getElementById("search-container").style.display = 'block ';
     else
-    document.getElementById("search-container").style.display = 'none ';
-  });
+        document.getElementById("search-container").style.display = 'none ';
+});
 
 
 // handle the swiper transition 
@@ -16,97 +16,90 @@ const carouselFirstChild = carousel.querySelector(".slider");
 const slides = carousel.querySelectorAll(".slide");
 const element = slides[slides.length - 1];
 const styles = window.getComputedStyle(element);
-let  widthValue = element.offsetWidth ;
+let widthValue = element.offsetWidth;
 let marginRight = parseInt(styles.marginRight);
 
 function startCarousel() {
 
-    let A_slides =  Array.from(slides);
-    let currentIndex = 0; 
+    let A_slides = Array.from(slides);
+    let currentIndex = 0;
     const screenWidth = window.innerWidth;
 
-    // if (screenWidth >= 1026) {
-    //     const lastSlide = A_slides[A_slides.length-1];
-    //     carouselFirstChild.insertBefore(lastSlide, carouselFirstChild.firstChild);
-    // } 
+    let width;
 
-    // let width;
-   
     function moveNext() {
-        if(screenWidth >= 1026){
+        if (screenWidth >= 1026) {
 
             width = currentIndex === 0 ? widthValue : widthValue + marginRight;
-        }else{
-            width =  widthValue + marginRight;
+        } else {
+            width = widthValue + marginRight;
         }
+
         A_slides[currentIndex].classList.remove("active");
-    
+
         carouselFirstChild.scrollLeft += width;
-    
-        
+
+
         const clonedSlide = A_slides[currentIndex].cloneNode(true);
         A_slides.push(clonedSlide);
         carouselFirstChild.appendChild(clonedSlide);
-        if(A_slides.length > slides.length + 2){
-            
+        if (A_slides.length > slides.length + 2) {
+
             carouselFirstChild.removeChild(A_slides[0]);
             carouselFirstChild.scrollLeft += -width;
             A_slides.shift();
             currentIndex--;
-            
-            
+
+
         }
 
-        currentIndex = (currentIndex + 1)  ;
+        currentIndex = (currentIndex + 1);
 
         A_slides[currentIndex].classList.add("active");
-    
+
     }
 
-    let intervalId = setInterval(moveNext, duration); 
-    
-    carousel.addEventListener("mouseenter", function() {
+    let intervalId = setInterval(moveNext, duration);
+
+    carousel.addEventListener("mouseenter", function () {
         clearInterval(intervalId);
     });
-    
-    carousel.addEventListener("mouseleave", function() {
-        intervalId = setInterval(moveNext, duration); 
-        
+
+    carousel.addEventListener("mouseleave", function () {
+        intervalId = setInterval(moveNext, duration);
+
     });
 
     const scrollRightBtn = document.getElementById("scrollRightBtn");
-    
 
-    scrollRightBtn.addEventListener("click", function() {
-        clearInterval(intervalId); 
-        moveNext()
-        intervalId = setInterval(moveNext, duration);
+
+    scrollRightBtn.addEventListener("click", function () {
+        moveNext();
     });
 
-    scrollLeftBtn.addEventListener("click", function() {
-        clearInterval(intervalId); 
-        for(let i = 0 ; i<slides.length - 1;i++) {
+    scrollLeftBtn.addEventListener("click", function () {
+
+        for (let i = 0; i < slides.length - 1; i++) {
             moveNext();
-            
+
         }
-        intervalId = setInterval(moveNext, duration);
+
     });
-    carouselFirstChild.addEventListener('click', function(event) {
-        // clearInterval(intervalId); 
+    carouselFirstChild.addEventListener('click', function (event) {
 
         const clickedSlide = event.target.closest('.slide');
         if (clickedSlide) {
             const index = Array.from(this.children).indexOf(clickedSlide);
-            let deff = (index - currentIndex ) > 0 ? (index - currentIndex ) : (slides.length  ) +(index - currentIndex);
-            console.log('deff',deff);
-            console.log('index',index);
-            console.log('currentIndex',currentIndex);
+            let deff = (index - currentIndex) > 0 ? (index - currentIndex) : (slides.length) + (index - currentIndex);
+            console.log('deff', deff);
+            console.log('index', index);
+            console.log('currentIndex', currentIndex);
 
-            for(let i = 0 ; i<deff;i++) {
+            for (let i = 0; i < deff; i++) {
                 moveNext();
             }
-           if(currentIndex > 1) carouselFirstChild.scrollLeft = width * 2;
-           else  carouselFirstChild.scrollLeft =width* currentIndex ;
+            if (currentIndex > 1) carouselFirstChild.scrollLeft = width * 2;
+            else carouselFirstChild.scrollLeft = width * currentIndex;
 
             // intervalId = setInterval(moveNext, duration);
 
@@ -114,8 +107,8 @@ function startCarousel() {
     });
 
 }
-if(slides.length > 1)
-document.addEventListener("DOMContentLoaded", startCarousel);
+if (slides.length > 1)
+    document.addEventListener("DOMContentLoaded", startCarousel);
 
 
 
